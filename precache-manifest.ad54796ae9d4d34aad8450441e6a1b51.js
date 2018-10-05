7f624069d5de929dd2df6a78d3d1cd5ccc9ed26c
mrfazry <mredykafazry@gmail.com>
2018-10-06 00:20:39 +0700

    Updates

---
+++ precache-manifest.ad54796ae9d4d34aad8450441e6a1b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89eaa7422b26701cdfabbdf4fde0e5ee",
+    "url": "/random-quote/static/media/twitter.89eaa742.svg"
+  },
+  {
+    "revision": "ae265bfad00735e37c33",
+    "url": "/random-quote/static/js/runtime~main.ae265bfa.js"
+  },
+  {
+    "revision": "99702111a18dd02be2f8",
+    "url": "/random-quote/static/js/main.99702111.chunk.js"
+  },
+  {
+    "revision": "aa7e2d68cb6773ab59b0",
+    "url": "/random-quote/static/js/1.aa7e2d68.chunk.js"
+  },
+  {
+    "revision": "99702111a18dd02be2f8",
+    "url": "/random-quote/static/css/main.dfeb48d3.chunk.css"
+  },
+  {
+    "revision": "1cce9c2942ee6be76ffaf05f1d24ea21",
+    "url": "/random-quote/index.html"
+  }
+];
